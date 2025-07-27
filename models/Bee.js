407b0d83d984755ec6a976f6cc67fed25c4714fe
notc0nnor <medea.lukic@gmail.com>
2025-07-28 00:47:53 +0200

    Update Bee.js

--- models/Bee.js
+++ models/Bee.js
@@ -4,7 +4,20 @@ const beeSchema = new mongoose.Schema({
   beeId: { type: String, required: true, unique: true }, // like "B123"
   ownerId: { type: String, required: true }, // Discord user ID
   xp: { type: Number, default: 0 },
-  ep: { type: Number, default: 0 }
+  ep: { type: Number, default: 0 },
+  adventure: {
+  type: {
+    type: String, // '1h', '3h', '8h'
+    default: null,
+  },
+  startedAt: {
+    type: Date,
+    default: null,
+  },
+  endsAt: {
+    type: Date,
+    default: null,
+  }}
 });
 
 module.exports = mongoose.model('Bee', beeSchema);
