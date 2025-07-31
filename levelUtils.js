37b94cdfdc54db61381e8891c2327a51cb0f44ac
notc0nnor <medea.lukic@gmail.com>
2025-07-31 15:25:13 +0200

    Update levelUtils.js

--- levelUtils.js
+++ levelUtils.js
@@ -51,7 +51,25 @@ function getXpLevel(xp) {
   return 0; // Default to level 0
 }
 
+function getXpLevelThreshold(level) {
+  const xpLevels = [
+    0,     // Level 0
+    50,    // Level 1
+    135,   // Level 2
+    255,   // Level 3
+    420,   // Level 4
+    600,   // Level 5
+    850,   // Level 6
+    1200,  // Level 7
+    1600,  // Level 8
+    1950,  // Level 9
+    2500   // Level 10+
+  ];
+  return xpLevels[level] ?? null;
+}
+
 module.exports = {
   getXpLevel,
   getEpLevel,
+  getXpLevelThreshold,
 };
