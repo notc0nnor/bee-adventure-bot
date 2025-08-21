3697159ac5b5441c99c85bbeab0487939d495c7e
notc0nnor <medea.lukic@gmail.com>
2025-08-21 22:15:13 +0200

    Bigupdate levelUtils.js

--- levelUtils.js
+++ levelUtils.js
@@ -1,78 +1,47 @@
-const epLevels = [
-  { ep: 0, name: 'Getting acquainted' },
-  { ep: 50, name: 'Friendly' },
-  { ep: 125, name: 'Affectionate' },
-  { ep: 225, name: 'Loyal' },
-  { ep: 350, name: 'Loving' },
-  { ep: 500, name: 'Bonded' },
+
+
+const levels = [
+  { ep: 0,    name: "Getting Acquainted" }, // Level 0
+  { ep: 50,   name: "Engaging" },           // Level 1
+  { ep: 135,  name: "Interested" },         // Level 2
+  { ep: 255,  name: "Connected" },          // Level 3
+  { ep: 420,  name: "Friendly" },           // Level 4
+  { ep: 600,  name: "Trusting" },           // Level 5
+  { ep: 850,  name: "Affectionate" },       // Level 6
+  { ep: 1200, name: "Loyal" },              // Level 7
+  { ep: 1600, name: "Loving" },             // Level 8
+  { ep: 1950, name: "Bonded" },             // Level 9
+  { ep: 2500, name: "Soulbound" },          // Level 10
 ];
 
-function getEpLevel(ep) {
+function getLevel(ep) {
   let currentLevel = 0;
-  let currentLevelObj = epLevels[0];
+  let currentLevelObj = levels[0];
   let nextThreshold = null;
 
-  for (let i = 1; i < epLevels.length; i++) {
-    if (ep >= epLevels[i].ep) {
+  for (let i = 1; i < levels.length; i++) {
+    if (ep >= levels[i].ep) {
       currentLevel = i;
-      currentLevelObj = epLevels[i];
+      currentLevelObj = levels[i];
     } else {
-      nextThreshold = epLevels[i].ep;
+      nextThreshold = levels[i].ep;
       break;
     }
   }
 
   return {
-    level: currentLevel,        // <-- this is what was missing
+    level: currentLevel,
     name: currentLevelObj.name,
     current: currentLevelObj.ep,
     next: nextThreshold
   };
 }
 
- 
-
-function getXpLevel(xp) {
-  const xpLevels = [
-    0,     // Level 0
-    50,    // Level 1
-    135,   // Level 2
-    255,   // Level 3
-    420,   // Level 4
-    600,   // Level 5
-    850,   // Level 6
-    1200,  // Level 7
-    1600,  // Level 8
-    1950,  // Level 9
-    2500   // Level 10+
-  ];
-
-  for (let i = xpLevels.length - 1; i >= 0; i--) {
-    if (xp >= xpLevels[i]) return i;
-  }
-
-  return 0; // Default to level 0
-}
-
-function getXpLevelThreshold(level) {
-  const xpLevels = [
-    0,     // Level 0
-    50,    // Level 1
-    135,   // Level 2
-    255,   // Level 3
-    420,   // Level 4
-    600,   // Level 5
-    850,   // Level 6
-    1200,  // Level 7
-    1600,  // Level 8
-    1950,  // Level 9
-    2500   // Level 10+
-  ];
-  return xpLevels[level] ?? null;
+function getLevelThreshold(level) {
+  return levels[level] ? levels[level].ep : null;
 }
 
 module.exports = {
-  getXpLevel,
-  getEpLevel,
-  getXpLevelThreshold,
+  getLevel,
+  getLevelThreshold,
 };
