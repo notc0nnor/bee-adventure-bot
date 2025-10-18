6eb0007a7f99e8d3cf6a7432e850d35267a6424c
notc0nnor <medea.lukic@gmail.com>
2025-10-18 22:11:44 +0200

    Create shop.js

---
+++ models/shop.js
@@ -0,0 +1,31 @@
+// shop/items.js
+module.exports = {
+  nectar: {
+    key: 'nectar',
+    name: 'Nectar',
+    emoji: '<:nectar:1389740460620382288>',
+    ep: 10,
+    cost: 100,
+  },
+  honey: {
+    key: 'honey',
+    name: 'Honey',
+    emoji: '<:Honey:1390088067947167885>',
+    ep: 15,
+    cost: 145,
+  },
+  beebread: {
+    key: 'beebread',
+    name: 'Bee Bread',
+    emoji: '<:BeeBread:1390098834192863232>',
+    ep: 20,
+    cost: 180,
+  },
+  gelee: {
+    key: 'gelee',
+    name: 'Gelee Royale',
+    emoji: '<:GeleeRoyale:1390091559302729889>',
+    ep: 35,
+    cost: 200,
+  },
+};
