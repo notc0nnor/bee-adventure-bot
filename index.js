546c2b01524740c2b61c9861fee8e902c827a7e8
notc0nnor <medea.lukic@gmail.com>
2026-01-03 23:21:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-Connect to MongoDB
+// Connect to MongoDB
   mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
